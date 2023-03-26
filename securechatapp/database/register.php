21b80b9955994a936945a894684fcfdfa0cf2816
Abhishekpat <104193075+Abhishekpat@users.noreply.github.com>
2023-03-26 12:28:19 +0530

    Update register.php

--- securechatapp/database/register.php
+++ securechatapp/database/register.php
@@ -12,7 +12,7 @@ $success_message = '';
 			header('location:chatroom.php');
 		}
 
-		require_once('database/chatuser.php');
+		require_once('securechatapp/database/chatuser.php');
 
 		$user_object = new chatuser;
 
@@ -28,7 +28,7 @@ $success_message = '';
 
 		$user_object->setUserStatus('Disabled');
 
-		$user_object->setUserCreatedOn(date(Y-m-d H:i:s));
+		$user_object->setUserCreatedOn(date('Y-m-d H:i:s'));
 
 		$user_object->setUserVerificationCode(md5(uniqid()));
 
@@ -54,6 +54,8 @@ $success_message = '';
 
 	}
 
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -61,9 +63,64 @@ $success_message = '';
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Sign-up form</title>
+
+	<!-- bootstarp core css -->
+	<link rel="stylesheet"  href="vendor-front/bootstrap/bootstrap.min.css">
+
+	<link rel="stylesheet" type="text/css" href="vendor-front/fontawesome-free/css/all.min.css">
+
+	<link rel="stylesheet" type="text/css" href="vendor-front/parsley/parsley.css">
+
+	<!--bootstrap core javascript-->
+	<script src="vendor-front/jquery/jquery.min.js"></script>
+
+	<script src="vendor-front/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+	<!--core plugin javascript-->
+	<script src="vendor-front/jquery-easing/jquery.easing.min.js"></script>
+
+	<script type="text/javascript" src="vendor-front/parsley/dist/parsley.min.js"></script>
 </head>
 <body>
-	<form  method="post" id="register_form">
+	
+
+	<div class="container">
+		<br/>
+		<br/>
+		<h1 class="text-center">Secure Chat App</h1>
+
+		<div class="row justify-content-md-center">
+			<div class="col col-md-4 mt-5">
+
+				<?php 
+
+				if($error!= '')
+				{
+					echo '<div class ="alert alert-warning alert-dismissible fade show" role="alert">
+					'.$error.'
+					<button type="button" class="close" data-dismissible="alert" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+					</button>
+					</div>
+					';
+				}
+
+				if($success_message!= '')
+				{
+					echo ' 
+					<div class ="alert alert-success">
+					'.$success_message.'
+					</div>
+
+					';
+				}
+
+				?>
+				<div class="card">
+				   <div class ="card-header">Register</div>
+				  <div class="card-body">
+
+				  	<form  method="post" id="register_form">
 		<div class="form-group">
 			<label >Enter Name</label>
 			<input type="text" name="user_name" id="user_name" class="form-control" data-parsley-pattern="/^[a-zA-Z\s]+$/" required/>
@@ -80,13 +137,24 @@ $success_message = '';
 		</div>
 
 		<div class="form-group text-center">
-			<input type="submit" name="register" class="btn btn-success" value="Register">
+			<input type="submit" name="register" class="btn btn-success" value="Register"/>
 		</div>
 	</form>
+
+				   </div>
+			   </div>
+			</div>
+		</div>
+	</div>
+
+
+
+
+
 </body>
 </html>
 
-?>
+
 
 <script>
 	$(document).ready(function(){
